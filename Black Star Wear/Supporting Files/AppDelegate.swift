cfb12ac87e8a95e6ec7795e96475a0a9bb49a8f5
Vladislav Len <vlad@1nd3e.ru>
2020-12-13 21:50:18 +0300

    Организовал структуру проекта

---
+++ Black Star Wear/Supporting Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Black Star Wear
+//
+//  Created by Vladislav Len on 13.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
